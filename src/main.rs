f101c0c4f83ab43136a78d64fa15fd5aa2115e25
martin-bucinskas <7595909+martin-bucinskas@users.noreply.github.com>
2020-10-05 00:36:31 +0100

    vga-test: Testing out writing to a VGA buffer.
    - Added an unsafe writing to VGA buffer operation to test out printing text to screen.
    - Added instructions on how to generate a bootable image.

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@
 
 use core::panic::PanicInfo;
 
+static HELLO_WORLD: &[u8] = b"Hello, world!";
+
 // Calls on panic
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -20,5 +22,14 @@ fn panic(_info: &PanicInfo) -> ! {
 // Don't mangle the entry point function name
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO_WORLD.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
